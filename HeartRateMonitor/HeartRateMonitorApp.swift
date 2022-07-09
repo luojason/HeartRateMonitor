148f39ab9dbf588dc22f941e5f37e02dfebf4bd5
Jason Luo <luojason1227@gmail.com>
2022-07-08 21:39:40 -0400

    Initial Commit

---
+++ HeartRateMonitor/HeartRateMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMonitorApp.swift
+//  HeartRateMonitor
+//
+//  Created by Jason Luo on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
